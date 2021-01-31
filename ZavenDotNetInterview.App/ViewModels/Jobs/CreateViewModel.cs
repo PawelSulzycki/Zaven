06b40f77213f91ddae31464a1d935c31031e1f10
Pawel Sulzycki <sulzyckipawel@gmail.com>
2021-01-31 19:45:45 +0100

    Removed RegularExpression from CreateViewModel

--- ZavenDotNetInterview.App/ViewModels/Jobs/CreateViewModel.cs
+++ ZavenDotNetInterview.App/ViewModels/Jobs/CreateViewModel.cs
@@ -6,7 +6,6 @@ namespace ZavenDotNetInterview.App.ViewModels.Jobs
     public class CreateViewModel
     {
         [Required(ErrorMessage = "The field is required")]
-        [RegularExpression(@"^\S*$", ErrorMessage = "No white space allowed")]
         public string Name { get; set; }
         public DateTime? DoAfter { get; set; }
     }
